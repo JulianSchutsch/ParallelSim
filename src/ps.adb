2517532ca13c64785088f1e887c7611a633df1bd
AlexanderC2 <asgardlogis@gmx.de>
2012-01-28 13:22:40 +0100

    modified ps.adb

--- src/ps.adb
+++ src/ps.adb
@@ -1,6 +1,28 @@
+pragma Ada_2005;
+
 with Endianess;
+with Types;
+with Network;
+with Network.Packets;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
+with BSDSockets;
 
 procedure Ps is
+
+   Socket : BSDSockets.SocketID;
+
 begin
-   null;
+
+   BSDSockets.Initialize;
+
+   Socket:=BSDSockets.Socket(AddressFamily => BSDSockets.AF_INET,
+                             SocketType    => BSDSockets.SOCK_STREAM,
+                             Protocol      => BSDSockets.IPPROTO_ANY);
+
+   BSDSockets.Bind(Socket => Socket,
+                   Port   => 1024);
+
+   BSDSockets.Finalize;
+
 end Ps;
