4247dfa22ee95a903d8e005d9a87c7066238d3e4
AlexanderC2 <asgardlogis@gmx.de>
2012-02-08 13:13:25 +0100

    Endian Types private

--- src/basics/endianess.ads
+++ src/basics/endianess.ads
@@ -29,19 +29,19 @@ with Interfaces;
 
 package Endianess is
 
-   type LittleEndianInteger16 is new Interfaces.Integer_16;
-   type LittleEndianInteger32 is new Interfaces.Integer_32;
-   type LittleEndianInteger64 is new Interfaces.Integer_64;
-   type LittleEndianCardinal16 is new Interfaces.Unsigned_16;
-   type LittleEndianCardinal32 is new Interfaces.Unsigned_32;
-   type LittleEndianCardinal64 is new Interfaces.Unsigned_64;
+   type LittleEndianInteger16 is private;
+   type LittleEndianInteger32 is private;
+   type LittleEndianInteger64 is private;
+   type LittleEndianCardinal16 is private;
+   type LittleEndianCardinal32 is private;
+   type LittleEndianCardinal64 is private;
 
-   type BigEndianInteger16 is new Interfaces.Integer_16;
-   type BigEndianInteger32 is new Interfaces.Integer_32;
-   type BigEndianInteger64 is new Interfaces.Integer_64;
-   type BigEndianCardinal16 is new Interfaces.Unsigned_16;
-   type BigEndianCardinal32 is new Interfaces.Unsigned_32;
-   type BigEndianCardinal64 is new Interfaces.Unsigned_64;
+   type BigEndianInteger16 is private;
+   type BigEndianInteger32 is private;
+   type BigEndianInteger64 is private;
+   type BigEndianCardinal16 is private;
+   type BigEndianCardinal32 is private;
+   type BigEndianCardinal64 is private;
 
    function ToLittleEndian(Integer: Integer16) return LittleEndianInteger16;
    function ToLittleEndian(Integer: Integer32) return LittleEndianInteger32;
@@ -58,5 +58,21 @@ package Endianess is
    function FromBigEndian(Integer: BigEndianInteger16) return Integer16;
    function FromBigEndian(Integer: BigEndianInteger32) return Integer32;
    function FromBigEndian(Integer: BigEndianInteger64) return Integer64;
+
+private
+
+   type LittleEndianInteger16 is new Interfaces.Integer_16;
+   type LittleEndianInteger32 is new Interfaces.Integer_32;
+   type LittleEndianInteger64 is new Interfaces.Integer_64;
+   type LittleEndianCardinal16 is new Interfaces.Unsigned_16;
+   type LittleEndianCardinal32 is new Interfaces.Unsigned_32;
+   type LittleEndianCardinal64 is new Interfaces.Unsigned_64;
+
+   type BigEndianInteger16 is new Interfaces.Integer_16;
+   type BigEndianInteger32 is new Interfaces.Integer_32;
+   type BigEndianInteger64 is new Interfaces.Integer_64;
+   type BigEndianCardinal16 is new Interfaces.Unsigned_16;
+   type BigEndianCardinal32 is new Interfaces.Unsigned_32;
+   type BigEndianCardinal64 is new Interfaces.Unsigned_64;
 
 end Endianess;
