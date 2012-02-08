01d257a7b7fc27b5bf70a768a414fc28457c1d45
AlexanderC2 <asgardlogis@gmx.de>
2012-02-08 01:52:18 +0100

    ps tests

--- src/ps.adb
+++ src/ps.adb
@@ -6,47 +6,56 @@ with Network;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Text_IO; use Ada.Text_IO;
 with BSDSockets;
-with BSDSockets.Packets;
 with CustomMaps;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 with Network.Streams;
 with Network.Barrier;
-with Network.Peers;
+with Network.Config;
 with BSDSockets.Streams;
 
+with SimControl;
+with SimElement;
+
 procedure Ps is
-   Server       : Network.Streams.ServerClassAccess;
-   ServerConfig : CustomMaps.StringStringMap.Map;
-   Client       : Network.Streams.ChannelClassAccess;
-   ClientConfig : CustomMaps.StringStringMap.Map;
+
+   NetworkImplementation: constant Network.Config.Implementation:=
+     (NewStreamServer  => BSDSockets.Streams.NewStreamServer'Access,
+      NewStreamClient  => BSDSockets.Streams.NewStreamClient'Access,
+      FreeStreamServer => BSDSockets.Streams.FreeStreamServer'Access,
+      FreeStreamClient => BSDSockets.Streams.FreeStreamClient'Access);
+
+   SimCtr : SimControl.SimControlAccess;
+   SimEle : SimElement.SimElementAccess;
+
+   Config : CustomMaps.StringStringMap.Map;
 
 begin
    BSDSockets.Initialize;
 
-   ServerConfig.Insert(To_Unbounded_String("Host"),To_Unbounded_String("127.0.0.1"));
-   ServerConfig.Insert(To_Unbounded_String("Port"),To_Unbounded_String("10001"));
-   ServerConfig.Insert(To_Unbounded_String("Family"),To_Unbounded_String("IPv4"));
+   Config.Insert
+     (To_Unbounded_String("ControlHost"),
+      To_Unbounded_String("127.0.0.1"));
+   Config.Insert
+     (To_Unbounded_String("ControlStreamPort"),
+      To_Unbounded_String("10001"));
+   Config.Insert
+     (To_Unbounded_String("IPFamily"),
+      To_Unbounded_String("IPv4"));
 
-   Server:=new BSDSockets.Streams.Server;
-   Server.Initialize(Config => ServerConfig);
+   SimCtr := SimControl.NewSimControl
+     (NetworkImplementation => NetworkImplementation,
+      Config                => Config);
 
-   ClientConfig.Insert(To_Unbounded_String("Host"),To_Unbounded_String("127.0.0.1"));
-   ClientConfig.Insert(To_Unbounded_String("Port"),To_Unbounded_String("10001"));
-   ClientConfig.Insert(To_Unbounded_String("Family"),To_Unbounded_String("IPv4"));
-
-   Client:=new BSDSockets.Streams.Client(Max=>1023);
-   Client.Initialize(Config => ClientConfig);
+   SimEle := SimElement.NewSimElement
+     (NetworkImplementation => NetworkImplementation,
+      Config                => Config);
 
    BSDSockets.Process;
+   BSDSockets.Streams.Process;
 
-
-   Client.Finalize;
-
-   Server.Finalize;
-
-   Network.Streams.Free(Client);
-   Network.Streams.Free(Server);
+   SimControl.FreeSimControl(SimCtr);
+   SimElement.FreeSimElement(SimEle);
 
    BSDSockets.Finalize;
 end Ps;
