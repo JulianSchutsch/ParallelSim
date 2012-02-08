080fb11adb1552649cd3fa6c859cf377fe2dd794
AlexanderC2 <asgardlogis@gmx.de>
2012-02-08 01:51:47 +0100

    simelement added

---
+++ src/simelement/simelement.adb
@@ -0,0 +1,67 @@
+-------------------------------------------------------------------------------
+--   Copyright 2012 Julian Schutsch
+--
+--   This file is part of ParallelSim
+--
+--   ParallelSim is free software: you can redistribute it and/or modify
+--   it under the terms of the GNU Affero General Public License as published
+--   by the Free Software Foundation, either version 3 of the License, or
+--   (at your option) any later version.
+--
+--   ParallelSim is distributed in the hope that it will be useful,
+--   but WITHOUT ANY WARRANTY; without even the implied warranty of
+--   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--   GNU Affero General Public License for more details.
+--
+--   You should have received a copy of the GNU Affero General Public License
+--   along with ParallelSim.  If not, see <http://www.gnu.org/licenses/>.
+-------------------------------------------------------------------------------
+with Ada.Unchecked_Deallocation;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
+package body SimElement is
+
+   procedure Free is new Ada.Unchecked_Deallocation
+     (Object => SimElement,
+      Name   => SimElementAccess);
+
+   function NewSimElement
+     (NetworkImplementation : Network.Config.Implementation;
+      Config                : CustomMaps.StringStringMap.Map)
+      return SimElementAccess is
+
+      Item : SimElementAccess;
+      StreamClientConfig : CustomMaps.StringStringMap.Map;
+
+   begin
+      Item := new SimElement;
+
+      Item.NetworkImplementation := NetworkImplementation;
+      Item.Config                := Config;
+
+      StreamClientConfig.Insert
+        (To_Unbounded_String("Host"),
+         Config.Element(To_Unbounded_String("ControlHost")));
+      StreamClientConfig.Insert
+        (To_Unbounded_String("Port"),
+         Config.Element(To_Unbounded_String("ControlStreamPort")));
+      StreamClientConfig.Insert
+        (To_Unbounded_String("Family"),
+         Config.Element(To_Unbounded_String("IPFamily")));
+
+      Item.StreamClient := NetworkImplementation.NewStreamClient
+        (Config => StreamClientConfig);
+
+      return Item;
+
+   end NewSimElement;
+   ---------------------------------------------------------------------------
+
+   procedure FreeSimElement
+     (Item : in out SimElementAccess) is
+   begin
+      Free(Item);
+   end FreeSimElement;
+   ---------------------------------------------------------------------------
+
+end SimElement;
---
+++ src/simelement/simelement.ads
@@ -0,0 +1,51 @@
+-------------------------------------------------------------------------------
+--   Copyright 2012 Julian Schutsch
+--
+--   This file is part of ParallelSim
+--
+--   ParallelSim is free software: you can redistribute it and/or modify
+--   it under the terms of the GNU Affero General Public License as published
+--   by the Free Software Foundation, either version 3 of the License, or
+--   (at your option) any later version.
+--
+--   ParallelSim is distributed in the hope that it will be useful,
+--   but WITHOUT ANY WARRANTY; without even the implied warranty of
+--   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--   GNU Affero General Public License for more details.
+--
+--   You should have received a copy of the GNU Affero General Public License
+--   along with ParallelSim.  If not, see <http://www.gnu.org/licenses/>.
+-------------------------------------------------------------------------------
+
+-- Revision History
+--   7.Feb 2012 Julian Schutsch
+--     - Original version
+pragma Ada_2005;
+
+with Network.Config;
+with Network.Streams;
+with CustomMaps;
+
+package SimElement is
+
+   type SimElement is tagged private;
+   type SimElementAccess is access SimElement;
+
+   function NewSimElement
+     (NetworkImplementation : Network.Config.Implementation;
+      Config                : CustomMaps.StringStringMap.Map)
+      return SimElementAccess;
+
+   procedure FreeSimElement
+     (Item : in out SimElementAccess);
+
+private
+
+   type SimElement is tagged
+      record
+         StreamClient          : Network.Streams.ClientClassAccess;
+         NetworkImplementation : Network.Config.Implementation;
+         Config                : CustomMaps.StringStringMap.Map;
+      end record;
+
+end SimElement;
