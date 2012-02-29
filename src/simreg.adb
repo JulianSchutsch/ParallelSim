098ddb1a642d300eacadf524c2cd8ed462329559
AlexanderC2 <asgardlogis@gmx.de>
2012-03-01 00:49:52 +0100

    simreg

---
+++ src/simreg.adb
@@ -0,0 +1,48 @@
+with Config;
+with Network.Config;
+pragma Warnings(OFF);
+with BSDSockets.Streams;
+with Network.Processes;
+pragma Warnings(ON);
+with ProgramArguments;
+with SimRegion;
+
+with GNAT.Traceback.Symbolic; use GNAT.Traceback.Symbolic;
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure simreg is
+
+   Configuration : Config.Config_Type;
+
+begin
+   ProgramArguments.Debug;
+
+   Network.Config.LoadConfiguration
+     (Configuration => Configuration);
+
+   Config.Debug
+     (Item => Configuration);
+
+   SimRegion.Initialize
+     (Configuration => Configuration);
+
+   loop
+      exit when SimRegion.Process;
+   end loop;
+
+   SimRegion.Finalize;
+exception
+   when E:others =>
+      Put("Exception Name : " & Exception_Name(E));
+      New_Line;
+      Put("Message : " & Exception_Message(E));
+      New_Line;
+      Put("Traceback      :");
+      New_Line;
+      Put(Symbolic_TraceBack(E));
+      New_Line;
+      loop
+         null;
+      end loop;
+end simreg;
