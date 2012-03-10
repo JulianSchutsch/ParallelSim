fcbcae7760270592bae003ff908a3853ef2b9897
AlexanderC2 <asgardlogis@gmx.de>
2012-03-10 18:46:27 +0100

    simclient

---
+++ src/simclient/simclient.adb
@@ -0,0 +1,13 @@
+package body SimClient is
+   procedure Initialize
+     (Configuration) is
+   begin
+      null;
+   end Initialize;
+
+   procedure Finalize is
+   begin
+      null;
+   end Finalize;
+
+end SimClient;
---
+++ src/simclient/simclient.ads
@@ -0,0 +1,5 @@
+package SimClient is
+   procedure Initialize
+     (Configuration);
+   procedure Finalize;
+end SimClient;
